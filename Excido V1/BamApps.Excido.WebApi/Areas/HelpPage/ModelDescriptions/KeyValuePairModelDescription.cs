06975e9d078f388fae02915bffbe6942ce1a2f21
Bill Fugina <bill@dogspots.com>
2015-10-05 01:48:03 +0000

    Add WebApi project

---
+++ Excido V1/BamApps.Excido.WebApi/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace BamApps.Excido.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
