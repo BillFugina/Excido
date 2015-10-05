81b33e9b506e7576665f5364446375c279908760
Bill Fugina <bill@dogspots.com>
2015-10-05 02:19:00 +0000

    Add Breeze Controller to WebApi

---
+++ BamApps.Excido.WebApi/Controllers/ExcidoBreezeController.cs
@@ -0,0 +1,35 @@
+﻿using BamApps.Excido.Data.Context;
+using BamApps.Excido.Data.Model;
+using Breeze.ContextProvider;
+using Breeze.ContextProvider.EF6;
+using Breeze.WebApi2;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BamApps.Excido.WebApi.Controllers {
+    [BreezeController]
+    public class ExcidoBreezeController : ApiController {
+        readonly EFContextProvider<ExcidoContext> _contextProvider = new EFContextProvider<ExcidoContext>();
+
+
+        [HttpGet]
+        public string Metadata() {
+            return _contextProvider.Metadata();
+        }
+
+        [HttpGet]
+        public IQueryable<SharedContentUnit> SharedContentUnits() {
+            return _contextProvider.Context.SharedContentUnits;
+        }
+
+        [HttpPost]
+        public SaveResult SaveChanges(JObject saveBundle) {
+            return _contextProvider.SaveChanges(saveBundle);
+        }
+    }
+}
