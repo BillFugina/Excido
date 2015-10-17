793e37cd70831ca6566672ebf06b2f5b109b9acb
Bill Fugina <bill@dogspots.com>
2015-10-17 19:49:03 +0000

    Use Aurelia Skeleton For WebApp

---
+++ BamAppsExcido.WebApp/gulpfile.js
@@ -0,0 +1,4 @@
+﻿var gulp = require('gulp'); gulp.task('copy-typings', function () {
+    return gulp.src('wwwroot/jspm_packages/**/*.d.ts')
+    .pipe(gulp.dest('scripts/typings/jspm_packages/'));
+});
