89712d52365457f7b1b626c15dcd32146c3f1ee8
Bill Fugina <bill@dogspots.com>
2015-10-17 19:49:03 +0000

    Use Aurelia Skeleton For WebApp

---
+++ Excido V1/BamAppsExcido.WebApp/scripts/typings/tsd.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="bootstrap/bootstrap.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="core-js/core-js.d.ts" />
+/// <reference path="whatwg-fetch/whatwg-fetch.d.ts" />
