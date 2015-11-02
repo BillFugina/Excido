07ad0d0dc53350c445f1ea2f08cd48db6bbeba71
Bill Fugina <bill@dogspots.com>
2015-11-02 04:09:42 +0000

    Angular App getting data from server!

---
+++ BamApps.Excido.Angular/app/BamApps/BamApps.Interface.ts
@@ -0,0 +1,20 @@
+﻿module BamApps {
+    export module Interface {
+
+        export interface ITitle {
+            title: string;
+        }
+
+        export interface IBreezeEntityManagerFactory {
+            newEntityManager: (hostName : string, servicePath : string) => ng.IPromise<breeze.EntityManager>;
+        }
+
+        export interface IRepository<T> {
+            getAll(): ng.IPromise<T[]>;
+        }
+
+        export interface IBreezeEntity {
+            source: string;
+        }
+    }
+}
