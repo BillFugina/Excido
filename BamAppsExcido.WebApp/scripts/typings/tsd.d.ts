8a9b9df79eb1e3e95f6cbb9cf54eb64e0abdc908
Bill Fugina <bill@dogspots.com>
2015-11-20 14:49:01 +0000

    Delete old web apps

--- BamAppsExcido.WebApp/scripts/typings/tsd.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="bootstrap/bootstrap.d.ts" />
-/// <reference path="jquery/jquery.d.ts" />
-/// <reference path="core-js/core-js.d.ts" />
-/// <reference path="whatwg-fetch/whatwg-fetch.d.ts" />
