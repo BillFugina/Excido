f05c552a745005cb037a35639d719dcc69ddaaa3
Bill Fugina <bill@dogspots.com>
2015-11-20 14:49:01 +0000

    Delete old web apps

--- Excido V1/BamAppsExcido.WebApp/gulpfile.js
+++
@@ -1,4 +0,0 @@
-﻿var gulp = require('gulp'); gulp.task('copy-typings', function () {
-    return gulp.src('wwwroot/jspm_packages/**/*.d.ts')
-    .pipe(gulp.dest('scripts/typings/jspm_packages/'));
-});
