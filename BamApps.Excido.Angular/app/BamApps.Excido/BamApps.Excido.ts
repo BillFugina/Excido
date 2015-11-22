2fe0d17818cafe2f07bd094bd485f4a87d9fc7d2
Bill Fugina <bill@dogspots.com>
2015-11-22 23:17:30 +0000

    Add tab and enter key handling

--- BamApps.Excido.Angular/app/BamApps.Excido/BamApps.Excido.ts
+++ BamApps.Excido.Angular/app/BamApps.Excido/BamApps.Excido.ts
@@ -4,6 +4,8 @@
         Logger.verbosity(Logger.Level.Log);
         export var app = angular.module('excido', ['breeze.angular']);
         app.directive("syncFocusWith", ["$timeout", "$rootScope", BamApps.Directive.SyncFocusDirective]);
+        app.directive("onEnterKey", ["$timeout", "$rootScope", BamApps.Directive.OnEnterKeyDirective]);
+        app.directive("onTabKey", ["$timeout", "$rootScope", BamApps.Directive.OnTabKeyDirective]);
         app.factory("entityManagerFactory", ["$q", BamApps.Service.breezeEntityManagerFactory]);
         app.factory("sharedContentUnitServiceFactory", ["$q", "entityManagerFactory", Excido.Service.SharedContentUnitServiceFactory]);
         app.controller("shared-units", ["$q", "sharedContentUnitServiceFactory", Excido.Controller.SharedUnitsController]);
--- BamApps.Excido.Angular/app/BamApps/BamApps.Directive.ts
+++ BamApps.Excido.Angular/app/BamApps/BamApps.Directive.ts
@@ -25,5 +25,51 @@
             }
         }
 
+        export function OnEnterKeyDirective($timeout: ng.ITimeoutService, $rootScope: ng.IRootScopeService): Interface.IEnterKeyDirective {
+            return {
+                restrict: "A",
+                link: link
+            }
+
+            function link(scope: Interface.ISyncFocusScope, element: ng.IAugmentedJQuery, attrs: Interface.IEnterKeyAttributes) {
+                element.bind("keydown keypress", function (event) {
+                    var keyCode = event.which || event.keyCode;
+
+                    // If enter key is pressed
+                    if (keyCode === 13) {
+                        scope.$apply(function () {
+                            // Evaluate the expression
+                            scope.$eval(attrs.onEnterKey);
+                        });
+
+                        event.preventDefault();
+                    }
+                });
+            }
+        }
+
+        export function OnTabKeyDirective($timeout: ng.ITimeoutService, $rootScope: ng.IRootScopeService): Interface.ITabKeyDirective {
+            return {
+                restrict: "A",
+                link: link
+            }
+
+            function link(scope: Interface.ISyncFocusScope, element: ng.IAugmentedJQuery, attrs: Interface.ITabKeyAttributes) {
+                element.bind("keydown keypress", function (event) {
+                    var keyCode = event.which || event.keyCode;
+
+                    // If tab key is pressed
+                    if (keyCode === 9) {
+                        scope.$apply(function () {
+                            // Evaluate the expression
+                            scope.$eval(attrs.onTabKey);
+                        });
+
+                        event.preventDefault();
+                    }
+                });
+            }
+        }
+
     }
 }
