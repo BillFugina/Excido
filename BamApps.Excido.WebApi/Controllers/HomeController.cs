c63a88338990e94a10620663cf4c80266d5b6265
Bill Fugina <bill@dogspots.com>
2015-11-25 15:16:15 +0000

    Redirect api root back to main website

--- BamApps.Excido.WebApi/Controllers/HomeController.cs
+++ BamApps.Excido.WebApi/Controllers/HomeController.cs
@@ -7,8 +7,13 @@ using System.Web.Mvc;
 namespace BamApps.Excido.WebApi.Controllers {
     public class HomeController : Controller {
         public ActionResult Index() {
-            ViewBag.Title = "Home Page";
-
+            Properties.Settings settings = new Properties.Settings();
+            if (Request.Url.IsLoopback) {
+                Response.Redirect(settings.LoopbackRootRedirect, true);
+            }
+            else {
+                Response.Redirect(settings.RootRedirect, true);
+            }
             return View();
         }
     }
