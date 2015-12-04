b626bc8ec1bf22180945a852949f852a5c8f9e8c
Bill Fugina <bill@dogspots.com>
2015-12-04 00:19:54 +0000

    Add logo choices page

---
+++ Excido V1/BamApps.Excido.Angular/Controllers/LogosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BamApps.Excido.Angular.Controllers
+{
+    public class LogosController : Controller
+    {
+        // GET: Logos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
