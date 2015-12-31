ff5fbb2f5e0d095e40b38250021ab35e034e8b4e
Bill Fugina <bill@dogspots.com>
2015-12-31 05:09:29 +0000

    UI Changes

--- BamApps.Excido.Angular/app/BamApps/BamApps.Filter.ts
+++ BamApps.Excido.Angular/app/BamApps/BamApps.Filter.ts
@@ -3,7 +3,7 @@
 
         export function CollapseFilter() {
 
-            return function (input: string, left: number = 10, middle: string = '...', right: number = 3) {
+            return function (input: string, left: number = 12, middle: string = '•••', right: number = 10) {
                 var result = input;
                 if (input.length > (left + middle.length + right)) {
                     result = input.substr(0, left) + middle + input.substr(input.length - right);
