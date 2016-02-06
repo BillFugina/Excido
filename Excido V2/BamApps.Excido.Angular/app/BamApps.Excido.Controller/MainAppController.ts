9f8641bd32c5fb085667de7acfd557a75e579fdf
Bill Fugina <bill@dogspots.com>
2016-02-06 19:41:40 +0000

    Style Login Page

--- Excido V2/BamApps.Excido.Angular/app/BamApps.Excido.Controller/MainAppController.ts
+++ Excido V2/BamApps.Excido.Angular/app/BamApps.Excido.Controller/MainAppController.ts
@@ -3,12 +3,16 @@
         export module Controller {
             export class MainAppController extends BamApps.Model.BamAppsBase implements BamApps.Excido.Interface.Controller.IMainAppController {
 
-                constructor(private $scope : ng.IScope , private settingsService: BamApps.Excido.Interface.ISettingsService) {
+                constructor(private $scope : ng.IScope , private settingsService: BamApps.Excido.Interface.ISettingsService, private $state : angular.ui.IStateService) {
                     super();
                 }
 
                 public get NameSpace (): string {
                     return this.settingsService.Settings.SlugPrefix;
+                }
+
+                public goHome(): void {
+                    this.$state.go('home')
                 }
 
             }
